1b978f6efbd68739c01f95b64fe9fa1b6587a7bb
JC <jc.jiechen.jane@gmail.com>
2024-03-30 22:10:28 -0500

    a5 Kanbas courses

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import express from "express";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 import cors from "cors";
+import CourseRoutes from "./Kanbas/courses/routes.js";
 
 const app = express();
 app.use(cors());
@@ -9,4 +10,6 @@ app.use(express.json());
 
 Hello(app);
 Lab5(app);
+CourseRoutes(app);
+
 app.listen(4000);
