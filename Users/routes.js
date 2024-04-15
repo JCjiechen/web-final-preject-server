23947f2376655e400f6dbf0cc63476f135c4ba8d
JC <jc.jiechen.jane@gmail.com>
2024-04-15 01:27:44 -0400

    a6 users

---
+++ Users/routes.js
@@ -0,0 +1,88 @@
+import * as dao from "./dao.js";
+// let currentUser = null;
+
+export default function UserRoutes(app) {
+  const createUser = async (req, res) => {
+    const user = await dao.createUser(req.body);
+    res.json(user);
+  };
+
+  const deleteUser = async (req, res) => {
+    const status = await dao.deleteUser(req.params.userId);
+    res.json(status);
+  };
+
+  const findAllUsers = async (req, res) => {
+    const { role } = req.query;
+    if (role) {
+      const users = await dao.findUsersByRole(role);
+      res.json(users);
+      return;
+    }
+    const users = await dao.findAllUsers();
+    res.json(users);
+  };
+
+  const findUserById = async (req, res) => {
+    const id = req.params.userId;
+    console.log(req.params);
+    const user = await dao.findUserById(id);
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).send(`User with id ${id} not found`);
+    }
+  };
+
+  const updateUser = async (req, res) => {
+    const { userId } = req.params;
+    const status = await dao.updateUser(userId, req.body);
+    const currentUser = await dao.findUserById(userId);
+    res.json(status);
+  };
+
+  const signup = async (req, res) => {
+    const user = await dao.findUserByUsername(req.body.username);
+    if (user) {
+      res.status(400).json({ message: "Username already taken" });
+    }
+    const currentUser = await dao.createUser(req.body);
+    req.session["currentUser"] = currentUser;
+    res.json(currentUser);
+  };
+
+  const signin = async (req, res) => {
+    const { username, password } = req.body;
+    const currentUser = await dao.findUserByCredentials(username, password);
+    if (currentUser) {
+      req.session["currentUser"] = currentUser;
+      res.json(currentUser);
+    } else {
+      res.status(401).send("Username or password is incorrect");
+    }
+  };
+
+  const signout = (req, res) => {
+    req.session.destroy();
+    res.sendStatus(200);
+  };
+
+  const profile = async (req, res) => {
+    const currentUser = req.session["currentUser"];
+    if (currentUser) {
+      res.json(currentUser);
+    } else {
+      res.status(403).send("Not logged in");
+    }
+  };
+
+  app.post("/api/users", createUser);
+  app.get("/api/users", findAllUsers);
+  app.get("/api/users/:userId", findUserById);
+  app.put("/api/users/:userId", updateUser);
+  app.delete("/api/users/:userId", deleteUser);
+  app.post("/api/users/signup", signup);
+  app.post("/api/users/signin", signin);
+  app.post("/api/users/signout", signout);
+  app.post("/api/users/profile", profile);
+}
